943b3606a2aff51f4a4dda868a9fd8959a12d804
rafaelNandes <rafael.fernandessilva@hotmail.com>
2021-07-15 23:25:54 -0300

    adding timepicker

--- app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
+++ app/src/main/java/com/example/todolist/ui/AddTaskActivity.kt
@@ -6,6 +6,8 @@ import com.example.todolist.databinding.ActivityAddTaskBinding
 import com.example.todolist.extensions.format
 import com.example.todolist.extensions.text
 import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import java.util.*
 
 class AddTaskActivity : AppCompatActivity() {
@@ -22,12 +24,21 @@ class AddTaskActivity : AppCompatActivity() {
     private fun insertListeners() {
         binding.inputLayoutDate.editText?.setOnClickListener {
             val datePicker = MaterialDatePicker.Builder.datePicker().build()
+
             datePicker.addOnPositiveButtonClickListener {
                 val timeZone = TimeZone.getDefault()
                 val offset = timeZone.getOffset(Date().time) * -1
                 binding.inputLayoutDate.text = Date(it + offset).format()
             }
             datePicker.show(supportFragmentManager, "DATE_PICKER_TAG")
+        }
+        binding.inputLayoutTime.editText?.setOnClickListener {
+            val timePicker = MaterialTimePicker.Builder().build()
+
+            timePicker.addOnPositiveButtonClickListener {
+                binding.inputLayoutTime.text = "${timePicker.hour}:${timePicker.minute}"
+            }
+            timePicker.show(supportFragmentManager, null)
         }
     }
 
